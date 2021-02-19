c348cfde3ff86a58b2ab69d41b304bf9e12d2a77
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2021-02-19 20:51:08 +0700

    first commit

---
+++ src/uas/if10k/pkg10119906/rizqyanandarusmana/no2/UASIF10K10119906RIZQYANANDARUSMANANO2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119906.rizqyanandarusmana.no2;
+
+/**
+ *
+ * @author senenngahenen
+ */
+public class UASIF10K10119906RIZQYANANDARUSMANANO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
